3994b0c2de96435064a71db6bc4795fa6eb1b993
GwonYeong <rnjsdud980@naver.com>
2024-12-04 18:00:36 +0900

    feat: express json

--- src/index.ts
+++ src/index.ts
@@ -3,13 +3,20 @@ import express from "express";
 const app = express();
 const port = 4000;
 
+app.use(express.json());
+
 app.get("/", (req, res) => {
   res.send("Hello, Express with TypeScript!");
 });
 
 app.post("/test", (req, res) => {
-  console.log(req.body);
+  console.log(req.body, "/test");
   res.send("Hello, Express with TypeScript!");
+});
+
+app.post("/test/json", (req, res) => {
+  console.log(req.body, "/test/json");
+  res.json({ message: "Hello, Express with TypeScript!" });
 });
 
 app.listen(port, () => {
